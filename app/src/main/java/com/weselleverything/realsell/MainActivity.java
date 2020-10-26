2fa81347a15d1c6fafc9a3a488a9bbe1f1e29f6e
Okoro Godwin Chinedu <chinedu.go@gmail.com>
2020-10-26 12:08:50 +0100

    work in progress!

--- app/src/main/java/com/weselleverything/realsell/MainActivity.java
+++ app/src/main/java/com/weselleverything/realsell/MainActivity.java
@@ -2,21 +2,26 @@ package com.weselleverything.realsell;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+    private MenuItem menuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +37,52 @@ public class MainActivity extends AppCompatActivity {
         /*----------------------Tool bar---------------------------*/
         setSupportActionBar(toolbar);
         /*----------------------Navigation Drawer Menu---------------------------*/
+
+        /*-------------------Hide or show items-------------------------*/
+//        Menu menu = navigationView.getMenu();
+//        menu.findItem(R.id.nav_profile).setVisible(true);
+//        menu.findItem(R.id.nav_view).setVisible(false);
+
+
+
+        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_home);
     }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else super.onBackPressed();
+    }
+
+
 
     public void logout (View view){
         FirebaseAuth.getInstance ().signOut (); //logging user out
         startActivity (new Intent(getApplicationContext (), Login.class));
         finish ();
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_shoe:
+                Intent intent = new Intent(MainActivity.this, shoes.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_share:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ app/src/main/java/com/weselleverything/realsell/shoes.java
@@ -0,0 +1,14 @@
+package com.weselleverything.realsell;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class shoes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoes);
+    }
+}
