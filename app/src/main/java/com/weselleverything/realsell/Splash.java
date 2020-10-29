2c5324e3d6c069bb408bb783df33e2633a6148e3
Okoro Godwin Chinedu <chinedu.go@gmail.com>
2020-10-29 04:39:07 +0100

    made a new splash screen!

--- app/src/main/java/com/weselleverything/realsell/Splash.java
+++ app/src/main/java/com/weselleverything/realsell/Splash.java
@@ -1,26 +1,55 @@
 package com.weselleverything.realsell;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 4000;
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView (R.layout.activity_splash);
-        getSupportActionBar ().hide ();
-        Handler hd = new Handler();
-        hd.postDelayed (new Runnable () {
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        // hooks
+        image = findViewById(R.id.imageView5);
+        logo = findViewById(R.id.textView3);
+        slogan = findViewById(R.id.textView4);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(Splash.this, Login.class);
-                startActivity (i);
-                finish ();
+                Intent intent = new Intent(Splash.this,Login.class);
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View,String>(image,"logo_image");
+                pairs[1] = new Pair<View,String>(logo,"logo_text");
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Splash.this,pairs);
+                startActivity(intent,options.toBundle());
+                finish();
             }
-        },3000);
+        },SPLASH_SCREEN);
     }
 }
