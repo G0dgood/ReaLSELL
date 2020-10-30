957ce753eeae7d6be0da0eed583d8fba353fd90a
Okoro Godwin Chinedu <chinedu.go@gmail.com>
2020-10-30 19:59:46 +0100

    just completed the navigation view! for camera

--- app/src/main/java/com/weselleverything/realsell/SearchActivity.java
+++ app/src/main/java/com/weselleverything/realsell/SearchActivity.java
@@ -1,16 +1,22 @@
 package com.weselleverything.realsell;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 import com.weselleverything.realsell.util.SectionsPagerAdapter;
 
 public class SearchActivity extends AppCompatActivity {
-    public static final String TAG = "SearchActivity";
+    private static final String TAG = "SearchActivity";
 
     //wadget
     public TabLayout mTablayout;
@@ -25,7 +31,7 @@ public class SearchActivity extends AppCompatActivity {
             setContentView(R.layout.activity_search);
             mTablayout = (TabLayout) findViewById(R.id.tabs);
             mViewPager = (ViewPager) findViewById(R.id.viewpager_container);
-            setupViewPager();
+            getVerifyPermissions();
         }
 
 
@@ -44,4 +50,28 @@ public class SearchActivity extends AppCompatActivity {
         mTablayout.getTabAt(3).setText(getString(R.string.fragment_account));
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        getVerifyPermissions();
+    }
+
+    public void getVerifyPermissions() {
+        Log.d(TAG, "verifyPermission: ask user for permission");
+        String[] Permissions = {Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Permissions[0]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Permissions[1]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Permissions[2]) == PackageManager.PERMISSION_GRANTED) {
+            setupViewPager();
+        }else {
+            ActivityCompat.requestPermissions(SearchActivity.this,Permissions,1) ;
+        }
+    }
+
+    private void verifyPermissions() {
+    }
 }
